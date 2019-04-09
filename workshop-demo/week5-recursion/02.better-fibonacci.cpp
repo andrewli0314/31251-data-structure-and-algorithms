9907b030b5c06eef29d7e782e59ccf645480d565
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-04-09 17:12:30 +1000

    Week 5 - Big Oh and Recursion

---
+++ workshop-demo/week5-recursion/02.better-fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+long fib_itr(long n){
+	
+	//Code goes here
+	
+	return 0;
+}
+
+int main(){
+	
+	std::cout << "Enter a number: " << std::endl;
+	
+	long n;
+	std::cin >> n;
+	
+	std::cout << "The nth Fibonacci number is: " << fib_itr(n) << std::endl;
+	
+}
